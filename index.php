79b833ed1e444a75880c24b04ea4ac0b7324a78d
Nobunaji <nobunajioda@gmail.com>
2022-11-23 17:17:56 +0100

    Style boutton like

--- index.php
+++ index.php
@@ -42,6 +42,7 @@
         @import url('https://fonts.googleapis.com/css2?family=Spartan:wght@500&display=swap');
     </style>
     <script defer src="/JS/theme.js"></script>
+    <script defer src="/JS/index.js"></script>
 </head>
 <body onload="active();onThemeSwitch();onAccentSwitch();">
     <!-- Main Content -->
@@ -98,7 +99,7 @@
                             }
                         }
                         if($status) { ?>
-                        <a <?php
+                        <a class="List_Item"<?php
                         $title = multiexplode(array(",", ":", "(", ")"), $Recettes[$i]['titre']);
                         $title2 = rtrim(slug($title[0]), "-");
                         echo "style='background-image:url(".'"'."/Photos/".strtolower($title2).".jpg".'"'.");'"; 
@@ -121,9 +122,11 @@
                                         <?php }
                                     ?>
                                     </ul>
-                                    <svg class="like" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
-                                    </svg>
+                                    <button class="like" onclick="Like();">
+                                        <svg width="25" height="25" viewBox="0 0 25 25" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
+                                        </svg>
+                                    </button>
                                 </div>
                             </div>
                         </a>
