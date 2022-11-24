ac099e95a67311ab3afeeabfcc29af60fd75f143
RomainEtu <romain.ly.etu@gmail.com>
2022-11-24 13:36:54 +0100

    Function to redirect user if not connected

---
+++ PHP/isAuth.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @param $url target url
+ * @return void
+ * Redirect user if not connected
+ */
+function isAuth($url): void {
+    if (isset($_SESSION['username'])) {
+        $host = $_SERVER['HTTP_HOST'];
+        $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+        echo "<script type='text/javascript'>window.top.location='http://". $host . $uri . "/" . $url ."';</script>";
+        exit;
+    }
+}
+
+
+?>
